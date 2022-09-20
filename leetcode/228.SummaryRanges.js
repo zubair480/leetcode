5a720f962c7a5c5e9b693d73873589892f6df57c
zubair480 <zubairzafar480@gmail.com>
2022-09-20 15:32:21 +0500

    31st commit

--- leetcode/228.SummaryRanges.js
+++ leetcode/228.SummaryRanges.js
@@ -14,3 +14,9 @@ const SummaryRanges = (nums) => {
 };
 
 console.log(SummaryRanges([0, 1, 2, 4, 5, 7]));
+
+/**
+ * We will iterate through the array and check if previous number plus 1 is equal to index of previous number plus 1 which is the next number
+ * if it is equal we will push it to the res array
+ * with a condition that it should return left plus current number
+ */
