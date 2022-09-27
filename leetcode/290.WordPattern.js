7f8471783c4ff586b238f77ef772c3ead90712b7
zubair480 <zubairzafar480@gmail.com>
2022-09-27 23:52:56 +0500

    40th commit

--- leetcode/290.WordPattern.js
+++ leetcode/290.WordPattern.js
@@ -22,3 +22,13 @@ var wordPattern = function (pattern, s) {
 };
 
 console.log(wordPattern("abba", "dog cat cat dog"));
+
+/**
+ * we will use two hashmaps because each alphabet of pattern is directly mapped to a word in the string.
+ * if the length of pattern is not equal to the length of string then we will return false.
+ * we will iterate over the string and check if the pattern is already mapped to a word or not.
+ * if it is not mapped then we will map it to the word.
+ * if the word is not mapped to any alphabet then we will map it to the alphabet.
+ * if the pattern is already mapped to a word and the word is not mapped to the alphabet then we will return false.
+ *
+ */
